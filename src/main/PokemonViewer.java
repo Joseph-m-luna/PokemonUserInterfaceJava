0d5739f7a3f4c0559a803922bf77e3e7311bd7dc
erikajledesma <erika.jasmine06@gmail.com>
2023-11-07 22:19:57 -0800

    experiment with separating name from pokemon info

--- src/main/PokemonViewer.java
+++ src/main/PokemonViewer.java
@@ -30,32 +30,44 @@ public class PokemonViewer {
 		// first pokemon panel
 		ImageIcon pokeImg1 = new ImageIcon(getClass().getResource("camera icon.png"));
 		JLabel pokeInfo1 = new JLabel("<html>Pokédex No. ###"
-				+ "<br>Name: "
 				+ "<br>Species: "
 				+ "<br>Type: "
 				+ "<br>Height: "
 				+ "<br>Weight: ", pokeImg1, SwingConstants.CENTER);
+
+		JLabel pokeName1 = new JLabel("Name", SwingConstants.CENTER);
+		pokeName1.setVerticalTextPosition(SwingConstants.BOTTOM);
+
 		firstPokemonPanel.add(pokeInfo1);
+		firstPokemonPanel.add(pokeName1);
 
 		// second pokemon panel
 		ImageIcon pokeImg2 = new ImageIcon(getClass().getResource("camera icon.png"));
 		JLabel pokeInfo2 = new JLabel("<html>Pokédex No. ###"
-				+ "<br>Name: "
 				+ "<br>Species: "
 				+ "<br>Type: "
 				+ "<br>Height: "
 				+ "<br>Weight: ", pokeImg2, SwingConstants.CENTER);
+		
+		JLabel pokeName2 = new JLabel("Name", SwingConstants.CENTER);
+		pokeName2.setVerticalTextPosition(SwingConstants.BOTTOM);
+
 		secondPokemonPanel.add(pokeInfo2);
+		secondPokemonPanel.add(pokeName2);
 
 		// third pokemon panel
 		ImageIcon pokeImg3 = new ImageIcon(getClass().getResource("camera icon.png"));
 		JLabel pokeInfo3 = new JLabel("<html>Pokédex No. ###"
-				+ "<br>Name: "
 				+ "<br>Species: "
 				+ "<br>Type: "
 				+ "<br>Height: "
 				+ "<br>Weight: ", pokeImg3, SwingConstants.CENTER);
+
+		JLabel pokeName3 = new JLabel("Name", SwingConstants.CENTER);
+		pokeName3.setVerticalTextPosition(SwingConstants.BOTTOM);
+
 		thirdPokemonPanel.add(pokeInfo3);
+		thirdPokemonPanel.add(pokeName3);
 
 		/// add to jpanel
 		jp.add(firstPokemonPanel);
